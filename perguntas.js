2ef370b2210f5913c811df9720c9ef5dc95ed4b4
Bananinha <anamanfrimteixeira@gmail.com>
2024-11-14 14:37:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,105 @@
+criaCartao(
+    '',
+    'Menor país do mundo:',
+    'Vaticano.'
+)
+
+criaCartao(
+    '',
+    'Maior país do mundo:',
+    'Rússia'
+)
+
+criaCartao(
+    '',
+    'Nacionalidade de Che Guevara:',
+    'Argentina.'
+)
+
+criaCartao(
+    '',
+    'Montanha mais alta do Brasil:',
+    'Pico da Neblina.'
+)
+
+criaCartao(
+    '',
+    'Local da Ásia em que o português é a língua oficial:',
+    'Macau.'
+)
+
+criaCartao(
+    '',
+    'País transcontinental:',
+    'Rússia.'
+)
+
+criaCartao(
+    '',
+    'Planetas do sistema solar:',
+    'Terra, Vênus, Saturno, Urano, Júpiter, Marte, Netuno, Mercúrio.'
+)
+
+criaCartao(
+    '',
+    'Maior animal terrestre:',
+    'Elefante Africano.'
+)
+
+criaCartao(
+    '',
+    'Qual o tema do discurso Eu tenho um Sonho, de Martin Luther King?',
+    'Igualdade de raças.'
+)
+
+criaCartao(
+    '',
+    'Com quais países o Equador faz fronteira?',
+    'Colômbia e Peru.'
+)
+
+criaCartao(
+    '',
+    'Qual animal gruguleja?',
+    'Peru.(glu glu)'
+)
+
+criaCartao(
+    '',
+    'Com quais países o Equador faz fronteira?',
+    'Colômbia e Peru.'
+)
+
+criaCartao(
+    '',
+    'Qual o oceano que cerca Madagascar?',
+    'Oceano Índico.'
+)
+
+criaCartao(
+    '',
+    'Quem pintou o teto da Capela Sistina?',
+    'Michelangelo.'
+)
+
+criaCartao(
+    '',
+    'Durante quantos anos, Fidel Castro esteve a frente de Cuba?',
+    '49 anos.'
+)
+
+criaCartao(
+    '',
+    'Quais pandemias mais afetaram o mundo todo?',
+    'Peste Negra e Covid-19.'
+)
+
+criaCartao(
+    '',
+    'Capital do Canadá:',
+    'Ottawa.'
+)
+
+
+
+
